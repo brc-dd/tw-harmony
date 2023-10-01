0824750b2ce324cd110c336f537673be692a30ae
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-10-01 15:59:17 +0530

    chore: fix build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,8 @@
-import fs from 'node:fs'
 import { defineConfig } from 'tsup'
 
-export default defineConfig({ entry: ['src/index.ts'], clean: true, dts: true })
-
-process.on('beforeExit', (code) => {
-  if (code === 0) {
-    fs.copyFileSync('dist/index.d.cts', 'dist/index.d.ts')
-    fs.writeFileSync('dist/index.js', `export * from "./index.cjs";\n`)
-  }
+export default defineConfig({
+  entry: ['src/index.ts'],
+  clean: true,
+  dts: true,
+  format: ['esm', 'cjs']
 })
