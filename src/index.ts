a06c4e8007afeb049a9fbb1859ab511b9fd9589c
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-10-07 19:52:20 +0530

    chore: prettify types

--- src/index.ts
+++ src/index.ts
@@ -4,50 +4,56 @@ export const colors = ({
   legacy = typeof process === 'undefined' || !process.env.NODE_ENV
 } = {}): Colors => (legacy ? rgb : oklch)
 
-type Colors = {
-  inherit: string
-  current: string
-  transparent: string
-  black: string
-  white: string
-} & Record<
-  | 'red'
-  | 'orange'
-  | 'amber'
-  | 'yellow'
-  | 'lime'
-  | 'green'
-  | 'emerald'
-  | 'teal'
-  | 'cyan'
-  | 'sky'
-  | 'blue'
-  | 'indigo'
-  | 'violet'
-  | 'purple'
-  | 'fuchsia'
-  | 'pink'
-  | 'rose'
-  | 'slate'
-  | 'gray'
-  | 'zinc'
-  | 'neutral'
-  | 'stone'
-  | 'sand'
-  | 'olive'
-  | 'mauve',
-  Record<
-    | '50'
-    | '100'
-    | '200'
-    | '300'
-    | '400'
-    | '500'
-    | '600'
-    | '700'
-    | '800'
-    | '900'
-    | '950',
-    string
+type Colors = Simplify<
+  {
+    inherit: string
+    current: string
+    transparent: string
+    black: string
+    white: string
+  } & Record<
+    | 'red'
+    | 'orange'
+    | 'amber'
+    | 'yellow'
+    | 'lime'
+    | 'green'
+    | 'emerald'
+    | 'teal'
+    | 'cyan'
+    | 'sky'
+    | 'blue'
+    | 'indigo'
+    | 'violet'
+    | 'purple'
+    | 'fuchsia'
+    | 'pink'
+    | 'rose'
+    | 'slate'
+    | 'gray'
+    | 'zinc'
+    | 'neutral'
+    | 'stone'
+    | 'sand'
+    | 'olive'
+    | 'mauve',
+    Simplify<
+      Record<
+        | '50'
+        | '100'
+        | '200'
+        | '300'
+        | '400'
+        | '500'
+        | '600'
+        | '700'
+        | '800'
+        | '900'
+        | '950',
+        string
+      >
+    >
   >
 >
+
+type Simplify<T> = { [K in keyof T]: T[K] } & {}
